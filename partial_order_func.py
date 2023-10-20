4cdb5084f21276faacb22190e974876adbe7d56b
JessieJ315 <58019628+JessieJ315@users.noreply.github.com>
2023-10-20 23:41:57 +0100

    Update partial_order_func.py

--- partial_order_func.py
+++ partial_order_func.py
@@ -20,6 +20,7 @@ def random_dag(num_actors:int, edge_probability:float, dag:bool=True)->np.ndarra
     np.fill_diagonal(binary_matrix,0)
     return binary_matrix
 
+
 def _matrix_exp_iterativ(matrix:np.ndarray)->np.ndarray:
   '''Iterator over a matrix.'''
   num_row = matrix.shape[0]
@@ -32,12 +33,14 @@ def _matrix_exp_iterativ(matrix:np.ndarray)->np.ndarray:
     idx = idx + 1
   return matrix_temp1
 
+
 def transitive_closure(partial_order:np.ndarray)->np.ndarray:
   '''Computes the transitive closure of an adjacency matrix.'''
   partial_order_updated = _matrix_exp_iterativ(partial_order)
   partial_order_updated = (partial_order_updated > 0).astype(int)
   np.fill_diagonal(partial_order_updated,0)
   return partial_order_updated
+
 
 def transitive_reduction(partial_order:np.ndarray)->np.ndarray:
   '''Computes the transitive reduction of an adjacency matrix.'''
@@ -50,3 +53,41 @@ def transitive_reduction(partial_order:np.ndarray)->np.ndarray:
           if (partial_order_closure[y,j]!=0):
             partial_order_closure[x,j]=0
   return partial_order_closure
+
+
+def depth(tr:np.ndarray)->int:
+  '''Calculates the depth of a partial order.
+
+  Args:
+    tr: The transitive reduction representation of partial order. 
+  
+  Returns:
+    The DAG depth of the partial order.
+  '''
+  num_items = tr.shape[0] 
+  if num_items==1:
+    return 1
+  col_sum = tr.sum(axis=0)
+  if col_sum.sum()==0:
+    return 1
+  col_sum_indicator = (col_sum==0)
+  row_sum = tr.sum(axis=1)
+  row_sum_indicator = (row_sum==0)
+  free_items = one(np.nonzero(row_sum_indicator & col_sum_indicator))
+  num_free_items = len(free_items)
+  if num_free_items == num_items:
+    return 1
+  if num_free_items > 0:
+    tr = np.delete(tr,free_items,0)
+    tr = np.delete(tr,free_items,1)
+    col_sum = tr.sum(axis=0)
+    col_sum_indicator = (col_sum==0)
+    row_sum = tr.sum(axis=1)
+    row_sum_indicator = (row_sum==0)
+  tops = one(np.nonzero(col_sum_indicator))
+  bottoms = one(np.nonzero(row_sum_indicator))
+  if len(bottoms)>len(tops):
+    tops = bottoms
+  tr = np.delete(tr,tops,0)
+  tr = np.delete(tr,tops,1)
+  return 1 + depth(tr)
